ef2690be0d800b4b52fafaf97013f77ab0b20619
mpmxyz <mpmxyz@users.noreply.github.com>
2015-05-25 18:55:53 +0200

    devfs: created a preview package

--- tarall.sh
+++ tarall.sh
@@ -15,4 +15,5 @@ tar --numeric-owner -cf tars/crunch.tar home/bin/crunch.lua home/lib/mpm/cache.l
 tar --numeric-owner -cf tars/pid.tar home/bin/pid.lua home/lib/pid.lua usr/man/pid.man
 #cbrowse.tar
 tar --numeric-owner -cf tars/cbrowse.tar home/bin/cbrowse.lua home/lib/mpm/tables.lua home/lib/mpm/draw_buffer.lua home/lib/mpm/component_filter.lua home/lib/mpm/config.lua home/lib/mpm/cache.lua home/lib/mpm/values.lua usr/man/cbrowse.man
-#devfs.tar: TODO
+#devfs.tar
+tar --numeric-owner -cf tars/devfs.tar boot/98_devfs.lua home/lib/devfs home/lib/drivers
