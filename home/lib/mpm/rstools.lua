cc25601b2f1b999cc7ecbc750efb132176cd60aa
mpmxyz <mpmxyz@users.noreply.github.com>
2016-01-29 15:12:02 +0100

    mpm.rstools: fixed some bugs, "address" can now be a proxy table, added documentation

--- home/lib/mpm/rstools.lua
+++ home/lib/mpm/rstools.lua
@@ -52,6 +52,12 @@ local function toAnalog(value)
   end
 end
 
+local function wrapTransform(f, input, output)
+  return function(...)
+    return output(f(input(...)))
+  end
+end
+
 local function append(value, a, ...)
   if a ~= nil then
     return a, append(value, ...)
@@ -60,25 +66,24 @@ local function append(value, a, ...)
   end
 end
 
-local function wrapGetterSetter(getter, setter, ...)
-  local args = table.pack(...)
+local function wrapGetterSetter(getter, setter, args)
   return function(value)
     if value == nil then
-      return getter(table.unpack(args, 1, args.n))
+      return getter(table.unpack(args))
     elseif setter then
-      return setter(append(value, table.unpack(args, 1, args.n)))
+      return setter(append(value, table.unpack(args)))
     else
       error("read only connection", 2)
     end
   end
 end
 
-local function wrapProxyFunctions(proxy, getterName, setterName, ...)
-  return wrapGetterSetter(proxy[getterName], proxy[setterName], ...)
+local function wrapProxyFunctions(proxy, getterName, setterName, args)
+  return wrapGetterSetter(proxy[getterName], proxy[setterName], args)
 end
 
-local function wrapAnalog(typ, address, side, color)
-  local proxy = component.proxy(address)
+local function wrapRaw(typ, address, side, color)
+  local proxy = (type(address) == "table") and address or component.proxy(address)
   local args, infix
   local getterName, setterName
   if side == "wireless" then
@@ -108,11 +113,13 @@ local function wrapAnalog(typ, address, side, color)
   if typ == "input" then
     getterName = "get#Input"
     setterName = "" --> becomes nil
-  else
+  elseif typ == "output" then
     getterName = "get#Output"
     setterName = "set#Output"
+  else
+    error("Expected type 'input' or 'output'.")
   end
-  return wrapProxyFunctions(proxy, getterName:gsub("#", infix), setterName:gsub("#", infix), table.unpack(args))
+  return wrapProxyFunctions(proxy, getterName:gsub("#", infix), setterName:gsub("#", infix), args)
 end
 
 local methods = {}
@@ -121,9 +128,11 @@ function methods:toggle()
   return self(invert(self()))
 end
 
-function methods:pulse(duration, value)
+function methods:pulse(duration, value, oldValue)
   checkArg(1, duration, "number")
-  local oldValue = self()
+  if oldValue == nil then
+    oldValue = self()
+  end
   if value == nil then
     value = invert(oldValue)
   end
@@ -132,18 +141,18 @@ function methods:pulse(duration, value)
   return self(oldValue)
 end
 
-
-local function newTable(f)
+local function newTable(f, typ)
+  local isOutput = (typ == "output")
   local normal = setmetatable({}, {
-    __index = methods,
+    __index = isOutput and methods or nil,
     __call = function(_, value)
       return f(value)
     end,
   })
   local inverted = setmetatable({}, {
-    __index = methods,
+    __index = isOutput and methods or nil,
     __call = function(_, value)
-      return inverted(f(inverted(value)))
+      return invert(f(invert(value)))
     end,
   })
   normal.inverted = inverted
@@ -151,30 +160,47 @@ local function newTable(f)
   return normal
 end
 
+--rstools.analog("input"/"output", address/proxy, side/"wireless", color) -> object
+--object(value) -> sets output
+--object() -> returns current input/output
+--object.inverted -> object with inverted inputs/outputs (only useful for digital interfaces)
+--object:toggle() -> toggles output                      (only useful for digital interfaces)
+--object:pulse(duration, value[, original]) -> overrides output value for 'duration' seconds, then restores original value
 function rstools.analog(typ, address, side, color)
   checkArg(1, typ, "string")
-  checkArg(2, address, "string")
+  checkArg(2, address, "string", "table")
   checkArg(3, side, "number", "string")
   checkArg(4, color, "number", "string", "nil")
-  local funcAnalog = wrapAnalog(typ, address, side, color)
-  local permittedType = (color == "all") and "table" or "boolean"
+  local func = wrapRaw(typ, address, side, color)
+  if side == "wireless" then
+    func = wrapTransform(func, toDigital, toAnalog)
+  end
+  local permittedType = (color == "all") and "table" or "number"
   return newTable(function(value)
     checkArg(1, value, permittedType, "nil")
-    value = funcAnalog(value)
-    return value
-  end)
+    return (func(value))
+  end, typ)
 end
 
+--rstools.analog("input"/"output", address/proxy, side/"wireless", color) -> object
+--object(value) -> sets output
+--object() -> returns current input/output
+--object.inverted -> object with inverted inputs/outputs (only useful for digital interfaces)
+--object:toggle() -> toggles output                      (only useful for digital interfaces)
+--object:pulse(duration, value[, original]) -> overrides output value for 'duration' seconds, then restores original value
 function rstools.digital(typ, address, side, color)
   checkArg(1, typ, "string")
-  checkArg(2, address, "string")
+  checkArg(2, address, "string", "table")
   checkArg(3, side, "number", "string")
   checkArg(4, color, "number", "string", "nil")
-  local funcAnalog = wrapAnalog(typ, address, side, color)
+  local func = wrapRaw(typ, address, side, color)
+  if side ~= "wireless" then
+    func = wrapTransform(func, toAnalog, toDigital)
+  end
   local permittedType = (color == "all") and "table" or "boolean"
   return newTable(function(value)
     checkArg(1, value, permittedType, "nil")
-    return toDigital(funcAnalog(toAnalog(value)))
+    return (func(value))
   end)
 end
 
